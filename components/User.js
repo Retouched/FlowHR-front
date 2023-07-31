c2fc5ab2dc6a3dde7527f5b725550cd85ce84ec3
Romain <pro.vuillaume@gmail.com>
2023-07-31 15:30:41 +0200

    Creation du fichier User.module.css

---
+++ components/User.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../styles/User.module.css";
+
+function User() {
+  return <div>Test</div>;
+}
+
+export default User;
