7b74214707740e50f479b3767df6b9e45631016e
Romain <pro.vuillaume@gmail.com>
2023-07-31 20:59:43 +0200

    Paramétrage des boutons ajouter un collaborateur et retour à la page d'accueil

--- components/ManageUsers.js
+++ components/ManageUsers.js
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../styles/ManageUsers.module.css";
 import User from "./User";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 function ManageUsers() {
   fetch("");
@@ -11,15 +13,21 @@ function ManageUsers() {
         <div className={styles.mainManagerUsers}>
           <div className={styles.addUserBtnAndTitle}>
             <button className={styles.addUserBtn} id="addUser">
+              <FontAwesomeIcon
+                icon={faUserPlus}
+                size="xl"
+                className={styles.userPlusIcon}
+              />
               Ajouter un collaborateur
             </button>
             <div className={styles.title}>Gestion des utilisateurs</div>
+            <div className={styles.whiteText}>Texte pour mis en page</div>
           </div>
           <div className={styles.allUsersContainer}>
             <User />
           </div>
-          <button className={styles.backBtn} id="">
-            Retour à la page d'accueil
+          <button className={styles.backBtn} id="back to HR dashboard">
+            RETOUR A LA PAGE D'ACCUEIL
           </button>
         </div>
       </div>
