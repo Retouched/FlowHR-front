70155c8b7e8d2c200406141a4a86049d2c2cb299
Romain <pro.vuillaume@gmail.com>
2023-08-01 13:04:30 +0200

    Avancement de la modal addUser : input text

--- components/ManageUsers.js
+++ components/ManageUsers.js
@@ -3,8 +3,20 @@ import styles from "../styles/ManageUsers.module.css";
 import User from "./User";
 import { faUserPlus, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
+import Modal from "./Modal";
+import useToggle from "./use-toggle";
 
 function ManageUsers() {
+  const [isModalAddUserOpen, toggleIsModalAddUserOpen] = useToggle(false);
+
+  const [addUserFirstname, setAddUserFirstname] = useState(null);
+  const [addUserLastname, setAddUserLastname] = useState(null);
+  const [addUserPassword, setAddUserPassword] = useState(null);
+  const [addUserEmail, setAddUserEmail] = useState(null);
+  const [addUserDepartment, setAddUserDepartment] = useState(null);
+  const [addUserJob, setAddUserJob] = useState(null);
+  const [addUserRole, setAddUserRole] = useState(null);
+
   const [usersData, setUsersData] = useState([]);
 
   useEffect(() => {
@@ -17,6 +29,27 @@ function ManageUsers() {
 
   console.log("usersData: ", usersData);
 
+  // AU CLIC SUR AJOUTER UN COLLABORATEUR
+  const handleAddUser = () => {
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstname: addUserFirstname,
+        lastname: addUserLastname,
+        //password: ,
+        //email: ,
+        //department: ,
+        //job: ,
+        //role: ,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   // MAP POUR AFFICHER L'ENSEMBLE DES USERS
   const users = usersData.map((data, i) => {
     console.log("data: ", data);
@@ -28,8 +61,61 @@ function ManageUsers() {
       <div className={styles.mainManageUsersContainer}>
         <div>NAVBAR</div>
         <div className={styles.mainManagerUsers}>
+          {isModalAddUserOpen && (
+            <Modal title="Add user" handleDismiss={toggleIsModalAddUserOpen}>
+              <div className={styles.modalContainer}>
+                <div>Creation d'un collaborateur</div>
+                <input
+                  type="text"
+                  placeholder="firstname"
+                  id="addUserFirstname"
+                  onChange={(e) => setAddUserFirstname(e)}
+                ></input>
+                <input
+                  type="text"
+                  placeholder="lastname"
+                  id="addUserLastname"
+                  onChange={(e) => setAddUserLastname(e)}
+                ></input>
+                <input
+                  type="text" // a changer en password pour mise en production
+                  placeholder="password"
+                  id="addUserPassword"
+                  onChange={(e) => setAddUserPassword(e)}
+                ></input>
+                <input
+                  type="text"
+                  placeholder="email"
+                  id="addUserEmail"
+                  onChange={(e) => setAddUserEmail(e)}
+                ></input>
+                <input
+                  type="text"
+                  placeholder="Pôle"
+                  id="addUserDepartment"
+                  onChange={(e) => setAddUserDepartment(e)}
+                ></input>
+                <input
+                  type="text"
+                  placeholder="Poste"
+                  id="addUserJob"
+                  onChange={(e) => setAddUserJob(e)}
+                ></input>
+                <input
+                  type="text"
+                  placeholder="Role"
+                  id="addUserRole"
+                  onChange={(e) => setAddUserRole(e)}
+                ></input>
+              </div>
+            </Modal>
+          )}
           <div className={styles.addUserBtnAndTitle}>
-            <button className={styles.addUserBtn} id="addUser">
+            <button
+              className={styles.addUserBtn}
+              id="addUser"
+              onClick={toggleIsModalAddUserOpen}
+            >
               <FontAwesomeIcon
                 icon={faUserPlus}
                 size="xl"
