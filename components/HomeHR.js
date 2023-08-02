2a5962c24a612b2fe58cc07bcec1619765448499
Quentin <orsini.quentin@gmail.com>
2023-08-02 09:56:09 +0200

    creat NavBar

--- components/HomeHR.js
+++ components/HomeHR.js
@@ -1,8 +1,10 @@
 import styles from "../styles/HomeHR.module.css";
+import NavBar from "./NavBar";
 
 function HomeHR() {
   return (
     <div>
+      <NavBar />
       <main className={styles.main}>
         <h1 className={styles.title}>HomeHR</h1>
       </main>
