eec79e26b32aed50eae0855e0be34cc2cffdbed3
Romain <pro.vuillaume@gmail.com>
2023-08-09 10:47:23 +0200

    Dynamiser la page manager : numRequest, date et nombre de choix en attente

--- components/HireRequestCard.js
+++ components/HireRequestCard.js
@@ -6,13 +6,36 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/HireRequestCard.module.css";
 
-function HireRequestCard() {
+function HireRequestCard(props) {
+  function formatDate(dateString) {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(dateString).toLocaleDateString("fr-FR", options);
+  }
+  function getHireRequestStatus(dpStatus, drhStatus, dafStatus, pdgStatus) {
+    const tabStatus = [dpStatus, drhStatus, dafStatus, pdgStatus];
+    const nbrOfChoiceLeft = 4;
+    for (let i = 0; i < tabStatus.length; i++) {
+      if (tabStatus[i] === "") {
+        nbrOfChoiceLeft -= 1;
+      }
+    }
+    return nbrOfChoiceLeft;
+  }
+
   return (
     <div className={styles.hireRequestContainer}>
       <div>ICON</div>
-      <div>Demande n°</div>
-      <div>jj/mm/aaaa</div>
-      <div>"x" validation(s) en attente</div>
+      <div>Demande n°{props.numRequest}</div>
+      <div>{formatDate(props.dateHireRequest)}</div>
+      <div>
+        {getHireRequestStatus(
+          props.dpRequestStatus,
+          props.drhRequestStatus,
+          props.dafRequestStatus,
+          props.pdgRequestStatus
+        )}{" "}
+        validation(s) en attente
+      </div>
     </div>
   );
 }
