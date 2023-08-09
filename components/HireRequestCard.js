b10e2e816f516b63816b495682be794330948978
Romain <pro.vuillaume@gmail.com>
2023-08-09 11:51:23 +0200

    Debugue de la fonction des choix restants plus conditionnement des icon en fonction du nombre de choix restant

--- components/HireRequestCard.js
+++ components/HireRequestCard.js
@@ -6,34 +6,55 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/HireRequestCard.module.css";
 
+import { useEffect, useState } from "react";
+
 function HireRequestCard(props) {
+  const [isDone, setIsDone] = useState(false);
+  const [nbrOfChoiceLeft, setNbrOfChoiceLeft] = useState(0);
+
   function formatDate(dateString) {
     const options = { year: "numeric", month: "2-digit", day: "2-digit" };
     return new Date(dateString).toLocaleDateString("fr-FR", options);
   }
+
+  useEffect(() => {
+    getHireRequestStatus(
+      props.dpRequestStatus,
+      props.drhRequestStatus,
+      props.dafRequestStatus,
+      props.pdgRequestStatus
+    );
+  }, []);
+
+  // FONCTION AFIN DE RECUPERER LE NBR DE CHOIX RESTANTS
   function getHireRequestStatus(dpStatus, drhStatus, dafStatus, pdgStatus) {
     const tabStatus = [dpStatus, drhStatus, dafStatus, pdgStatus];
-    const nbrOfChoiceLeft = 4;
+    const nbrOfChoiceLeft = tabStatus.length;
     for (let i = 0; i < tabStatus.length; i++) {
-      if (tabStatus[i] === "") {
+      console.log("tabStatus: ", tabStatus);
+      if (tabStatus[i] === true) {
         nbrOfChoiceLeft -= 1;
+        console.log("nbr: ", nbrOfChoiceLeft);
+        if (nbrOfChoiceLeft === 0) {
+          setIsDone(true);
+        }
       }
     }
-    return nbrOfChoiceLeft;
+    setNbrOfChoiceLeft(nbrOfChoiceLeft);
   }
 
   return (
     <div className={styles.hireRequestContainer}>
-      <div>ICON</div>
+      {isDone && (
+        <div className={styles.error}>
+          <FontAwesomeIcon icon={faCircleCheck} />
+        </div>
+      )}
+      {!isDone && <FontAwesomeIcon icon={faHourglassHalf} size="xl" />}
       <div>Demande n°{props.numRequest}</div>
       <div>{formatDate(props.dateHireRequest)}</div>
       <div>
-        {getHireRequestStatus(
-          props.dpRequestStatus,
-          props.drhRequestStatus,
-          props.dafRequestStatus,
-          props.pdgRequestStatus
-        )}{" "}
+        {nbrOfChoiceLeft}
         validation(s) en attente
       </div>
     </div>
